58112bf193a350087077dcb0701c0fc14069ff4f
qrelix <kurelagnieszka@gmail.com>
2017-09-15 04:27:22 +0200

    your commit message

---
+++ Przepisy/js/modals.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#login").click(function() {
+        $("#loginModal").modal();
+    });
+});
+
+
+$(document).ready(function() {
+    $("#signup").click(function() {
+        $("#signupModal").modal();
+    });
+});
+
+$(document).ready(function() {
+    $("#search").click(function() {
+        $("#searchModal").modal();
+    });
+});
